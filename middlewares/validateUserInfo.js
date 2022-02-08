874e73784687d88e4da52d80b0f1215bfdf7c9f0
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-07 21:02:33 -0300

    Requisito 1 - Refatora validação de dados do usuário

--- middlewares/validateUserInfo.js
+++ middlewares/validateUserInfo.js
@@ -1,18 +1,11 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
-  displayName: Joi.string().min(8).required().messages({
-    'any.required': '{{#label}} is required',
-    'string.empty': '{{#label}} length must be at least 8 characters long',
-    'string.min': '{{#label}} length must be at least 8 characters long',
-  }),
+  displayName: Joi.string().min(8).required(),
   email: Joi.string().email().required().messages({
-    'any.required': '{{#label}} is required',
     'string.email': '{{#label}} must be a valid email',
   }),
   password: Joi.string().min(6).required().messages({
-    'any.required': '{{#label}} is required',
-    'string.empty': '{{#label}} length must be 6 characters long',
     'string.min': '{{#label}} length must be 6 characters long',
   }),
   image: Joi.string(),
