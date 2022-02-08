75f11177daa4beedf0b93de01f50218ffe5a72f2
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-08 16:20:00 -0300

    Requisito 2 - Cria controller de login do usuário

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,17 @@ const create = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await userService.login({ email, password });
+    return res.status(200).json({ token });
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+};
+
 module.exports = {
   create,
+  login,
 };
