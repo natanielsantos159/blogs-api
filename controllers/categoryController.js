6f3eead350d178ca8da0d72d51a9a5aef7dadd30
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-09 22:37:32 -0300

    Requisito 6 - Cria controller categoryController.getAll

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -14,6 +14,16 @@ const create = async (req, res) => {
   }
 };
 
+const getAll = async (_req, res) => {
+  try {
+    const categories = await categoryService.getAll();
+    return res.status(200).json(categories);
+  } catch (err) {
+    return res.status(500).json({ message: 'Ocorreu um erro :(' });
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
